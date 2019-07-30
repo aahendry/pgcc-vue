89783eca6aec50d04747ba57220c940bd2f8c279
aahendry <aahendry@hotmail.com>
2019-07-30 15:33:52 +0100

    Working on home page.

---
+++ src/components/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+  <b-navbar toggleable="md" type="dark" variant="info">
+    <b-navbar-brand :to="{ name: 'Home' }">PGCC</b-navbar-brand>
+    <b-navbar-nav>
+      <b-nav-item href="#">Rinks</b-nav-item>
+      <b-nav-item href="#">Fixtures & Results</b-nav-item>
+      <b-nav-item-dropdown
+        id="nav-dropdown-competitions"
+        text="Competitions"
+        toggle-class="nav-link-custom"
+        right
+      >
+        <b-dropdown-item href="#">Gourdie</b-dropdown-item>
+        <b-dropdown-item href="#">Derby</b-dropdown-item>
+      </b-nav-item-dropdown>
+      <b-nav-item-dropdown
+        id="nav-dropdown-archive"
+        text="Archive"
+        toggle-class="nav-link-custom"
+        right
+      >
+        <b-dropdown-item href="#">Old Results</b-dropdown-item>
+        <b-dropdown-item href="#">Winning Rinks</b-dropdown-item>
+        <b-dropdown-item href="#">Photo Gallery</b-dropdown-item>
+      </b-nav-item-dropdown>
+      <b-nav-item href="#">Merchandise</b-nav-item>
+    </b-navbar-nav>
+  </b-navbar>
+</template>
+<script>
+export default {
+  name: "Navbar"
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import BootstrapVue from 'bootstrap-vue';
 import Vue from 'vue';
 import App from './App';
 import router from './router';
 
 Vue.config.productionTip = false;
+
+Vue.use(BootstrapVue);
 
 /* eslint-disable no-new */
 new Vue({
