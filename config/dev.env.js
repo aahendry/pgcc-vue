3eaf2749d17dc574dbbe016b69d6de1947ed7308
Andrew Hendry <andrew.hendry@leidos.com>
2019-10-14 13:55:53 +0100

    Updated npm packages.

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_ENDPOINT: '"https://localhost:5001/api"'
+  API_ENDPOINT: '"https://localhost:44300/api"'
 })
