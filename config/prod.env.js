ee48db909ea3594c9bfbfb142b200d5a94b6ae25
Andrew Hendry <andrew.hendry@leidos.com>
2019-10-16 17:32:09 +0100

    Updated prod config.

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  API_ENDPOINT: '"https://portglasgowcurlingclub.co.uk/api"'
+  API_ENDPOINT: '"https://api.portglasgowcurlingclub.co.uk"'
 }
