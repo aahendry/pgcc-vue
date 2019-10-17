286000e1d24e4578771938ab2549517757fb974a
Andrew Hendry <andrew.hendry@leidos.com>
2019-10-17 12:08:45 +0100

    Fixed incorrect API endpoint for prod.

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  API_ENDPOINT: '"https://api.portglasgowcurlingclub.co.uk"'
+  API_ENDPOINT: '"https://api.portglasgowcurlingclub.co.uk/api"'
 }
